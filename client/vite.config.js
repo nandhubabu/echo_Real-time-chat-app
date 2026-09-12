189a4d99c4ca2ba265a11187c92bc9c5f588134d
nandhubabu <nandhubabuvktd@gmail.com>
2026-09-12 16:26:54 +0530

    update proxy target for docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,11 +11,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://backend:5000",
         changeOrigin: true,
       },
       "/socket.io": {
-        target: "http://localhost:5000",
+        target: "http://backend:5000",
         ws: true,
       },
     },
